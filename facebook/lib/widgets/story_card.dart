8b5fc9a9034fe46338aa1f66ee11525c9fad8b13
github <iancarlonapone@gmail.com>
2025-10-03 10:51:59 +0800

    add story card

---
+++ facebook/lib/widgets/story_card.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StoryCard extends StatelessWidget {
+  const StoryCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
